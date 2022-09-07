6042500800010849e0a917510d85b4516d5d9f1e
Mateusz Matejko <matejko.mateusz7456@gmail.com>
2022-09-08 01:55:47 +0200

    history base file

---
+++ project/manager.py
@@ -0,0 +1,12 @@
+class Menager:
+    def __init__(self):
+        self.account = 0
+        self.history = []
+        self.buy = {}
+        self.sell = {}
+        self.overview = {}
+        self.stock = {}
+
+    def assign(self, funct):
+        def decorate(funct)
+            self.ac
