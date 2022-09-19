304af07cdee8489dd580d46a7ca237adef373eb8
Mateusz Matejko <matejko.mateusz7456@gmail.com>
2022-09-19 23:38:22 +0200

    funcitonality added

--- project/app.py
+++ project/app.py
@@ -4,17 +4,17 @@ from magazyn import main as magazyn_main
 from saldo import main as saldo_main
 from zakup import main as zakup_main
 from sprzedaz import main as sprzedaz_main
+from przeglad import main as przeglad_main
+
 
 app = Flask(__name__)
 
-stock = magazyn_main("history.txt")
-account = konto_main("history.txt")
 file = "history.txt"
 
 
 @app.route("/")
 def home():
-    return render_template("home.html", account=account, stock=stock)
+    return render_template("home.html", account=konto_main(file), stock=magazyn_main(file))
 
 
 @app.route("/action", methods=["GET", "POST"])
@@ -27,7 +27,6 @@ def check():
         result = saldo_main(action=action, change=change, comment=comment, file=file)
         if result:
             return render_template("error.html", msg=result)
-        saldo_main(action=action, change=change, comment=comment, file=file)
         return redirect("/")
     elif request.form.get("method") == "zakup":
         action = request.form.get("method")
@@ -38,32 +37,23 @@ def check():
         result = zakup_main(action=action, product_id=product_id, buy_price=buy_price, buy_qty=buy_qty, file=file)
         if result:
             return render_template("error.html", msg=result)
-        zakup_main(action=action, product_id=product_id, buy_price=buy_price, buy_qty=buy_qty, file=file)
         return redirect("/")
     elif request.form.get("method") == "sprzedaz":
         action = request.form.get("method")
         product_id = request.form.get("product_id")
-        sell_price = request.form.get("buy_price")
-        sell_qty = request.form.get("buy_qty")
+        sell_price = request.form.get("sell_price")
+        sell_qty = request.form.get("sell_qty")
         sell_price, sell_qty = int(sell_price), int(sell_qty)
         result = sprzedaz_main(action=action, product_id=product_id, sell_price=sell_price, sell_qty=sell_qty, file=file)
         if result:
             return render_template("error.html", msg=result)
-        sprzedaz_main(action=action, product_id=product_id, sell_price=sell_price, sell_qty=sell_qty, file=file)
         return redirect("/")
     return render_template("error.html", msg="check function error")
 
 
-# @app.route("/status/", methods=["POST", "GET"])
-# def status():
-#     r = ba()
-#     if not r:
-#         r = buy_status()
-#         if not r:
-#             r = sell_status()
-#             if not r:
-#                 return render_template("error.html")
-#     return render_template("success.html")
+@app.route("/history")
+def history():
+    return render_template("history.html", history=przeglad_main(file))
 
 
 
--- project/przeglad.py
+++ project/przeglad.py
@@ -2,12 +2,14 @@ import sys
 from manager import man_obj
 
 
-def main():
-    if len(sys.argv) != 2:
-        raise KeyError("2 arguments should be passed <file_name> <file_history>")
-    man_obj.get_history(sys.argv[1])
-    for transaction in man_obj.history:
-        print(transaction)
+def main(file):
+    # if len(sys.argv) != 2:
+    #     raise KeyError("2 arguments should be passed <file_name> <file_history>")
+    man_obj.get_history(file)
+    man_obj.write_history(file)
+    print(man_obj.history)
+    return man_obj.history
+
 
 
 if __name__ == '__main__':
