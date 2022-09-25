595f0cad756dbd3edb910be3432a31b247cc7504
Mateusz Matejko <matejko.mateusz7456@gmail.com>
2022-09-25 22:29:56 +0200

    task upgraded for database usage for files making actions

--- project/saldo.py
+++ project/saldo.py
@@ -2,15 +2,9 @@ import sys
 from manager import man_obj
 
 
-def main(action, change, comment, file):
-    # if len(sys.argv) != 4:
-    #     raise KeyError("4 arguments should be passed <file_name> <file_history> <change> <commentary>")
-    # file = sys.argv[1]
-    # change = int(sys.argv[2])
-    # comment = sys.argv[3]
-    man_obj.get_history(file)
+def main(action, change, comment):
+    man_obj.get_history()
     man_obj.execute(action=action, change=change, comment=comment)
-    man_obj.write_history(file=file)
     # return "Balance successfully changed. Current balance:"
 
 
--- project/zakup.py
+++ project/zakup.py
@@ -2,18 +2,10 @@ import sys
 from manager import man_obj
 
 
-def main(action, product_id, buy_price, buy_qty, file):
-    # if len(sys.argv) != 5:
-    #     raise KeyError("5 arguments should be passed <file_name> <file_history> <product_id> <price> <qty>")
-    # action = "zakup"
-    # file = sys.argv[1]
-    # product_id = sys.argv[2]
-    # buy_price = int(sys.argv[3])
-    # buy_qty = int(sys.argv[4])
-    man_obj.get_history(file)
+def main(action, product_id, buy_price, buy_qty):
+    man_obj.get_current_company()
     man_obj.execute(action=action, product_id=product_id, buy_price=buy_price, buy_qty=buy_qty)
-    man_obj.write_history(file=file)
-    # return f"{buy_qty}pcs of \"{product_id}\" successfully bought. Current stock:"
+    man_obj.get_current_company()
 
 
 if __name__ == '__main__':
